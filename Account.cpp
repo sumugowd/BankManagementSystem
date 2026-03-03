aeb982a8678f11cf6fe8a06e0382a17014c690e6
Sumanth <sumugowd@gmail.com>
2026-03-03 23:15:20 +0530

    Added multiple support using vector and implemented getter function

--- Account.cpp
+++ Account.cpp
@@ -11,11 +11,37 @@ Account::Account() {
 void Account::createAccount() {
     cout << "Enter Account Number: ";
     cin >> accountNumber;
+
+    cin.ignore();
+
+    cout << "Enter Name: ";
+    getline(cin,name);
+
+    cout << "Enter balance: ";
+    cin >> balance;
+    cout << "\n Account Created Successfully!\n";
 }
 
 // Display
 void Account::display() const {
     cout << "Account Number: " << accountNumber << endl;
     cout << "Name: " << name << endl;
-    cout << "Balance: ₹" << balance << endl;
+    cout << "Balance: Rs" << balance << endl;
+}
+
+void Account::deposit(double amount) {
+    balance += amount;
+    cout << "Amount Deposited Successfully\n";
+}
+
+int Account::getAccountNumber() const {
+    return accountNumber;
+}
+
+double Account::getBalance() const {
+    return balance;
+}
+
+string Account::getName() const {
+    return name;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,75 @@
 #include "Account.h"
+#include <vector>
 
 int main() {
-    Account acc;
-    acc.createAccount();
-    acc.display();
-    return 0;
+    vector<Account> accounts;
+    int choice;
+    int accNumber;
+    double amount;
+
+    while (true) {
+        cout << "\n====== BANK MANAGEMENT SYSTEM ======\n";
+        cout << "1. Create Account\n";
+        cout << "2. Deposit Money\n";
+        cout << "3. Display Account\n";
+        cout << "4. Exit\n";
+        cout << "Enter Choice: ";
+        cin >> choice;
+
+        switch (choice) {
+
+            case 1: {
+                Account acc;
+                acc.createAccount();
+                accounts.push_back(acc);
+                break;
+            }
+
+            case 2: {
+                cout << "Enter Account Number: ";
+                cin >> accNumber;
+
+                bool found = false;
+                for (auto &acc : accounts) {
+                    if (acc.getAccountNumber() == accNumber) {
+                        cout << "Enter Amount: ";
+                        cin >> amount;
+                        acc.deposit(amount);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                    cout << "Account Not Found\n";
+                break;
+            }
+
+            case 3: {
+                cout << "Enter Account Number: ";
+                cin >> accNumber;
+
+                bool found = false;
+                for (auto &acc : accounts) {
+                    if (acc.getAccountNumber() == accNumber) {
+                        acc.display();
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                    cout << "Account Not Found\n";
+
+                break;
+            }
+
+            case 4:
+                cout << "Exiting Program...\n";
+                return 0;
+
+            default:
+                cout << "Invalid Choice\n";
+        }
+    }
 }
